3a9c36a8f3811ab93606d51f04ee7893e4d34926
Melissa Nolan <125099416+MelissaNolan@users.noreply.github.com>
2024-11-02 10:01:52 -0700

    Add files via upload

--- consensus_and_profile.py
+++ consensus_and_profile.py
@@ -1,35 +1,31 @@
-with open("/Users/melissanolan/Downloads/rosalind_cons-2.txt") as file:
+with open("/Users/melissanolan/Downloads/rosalind_cons-3.txt") as file:
     data=file.readlines()
 
+#Create a list of sequences
 matrix=[]
 s=""
 for a in data:
     if ">" in a:
-        matrix.append(list(s))
+        if s:
+            matrix.append(list(s))
         s=""
         continue
     else:
-        s+=a.strip()
-matrix.append(list(s)) #to add the last sequenxe in
+        s += a.strip()
+if s:   #add last sequence
+    matrix.append(list(s)) 
 
-matrix.pop(0)
-#print(matrix)
-#matrix assembled
 columns=len(matrix[0])
 rows=len(matrix)
 
 #print(columns, rows) 
 
-a=t=c=g=0
-A=[]
-T=[]
-C=[]
-G=[]
+A, T, C, G = [], [], [], []
 
 
 for i in range(columns):
-    j=0
-    while j<(rows):
+    a=t=c=g=0
+    for j in range(rows):
         if matrix[j][i] =="A":
             a+=1
         elif matrix[j][i] =="T":
@@ -38,34 +34,21 @@ for i in range(columns):
             c+=1
         elif matrix[j][i] =="G":
             g+=1
-        j+=1
-    else: #when j>rows
-        A.append(a)
-        T.append(t)
-        C.append(c)
-        G.append(g)
-        a=t=c=g=0
-
+    A.append(a)
+    T.append(t)
+    C.append(c)
+    G.append(g)
 
 consensus=""
 for k in range(columns):
-    if (A[k]>T[k]) and (A[k]>C[k]) and (A[k]>G[k]):
-        consensus+= "A"
-    elif (C[k]>T[k]) and (C[k]>A[k]) and (C[k]>G[k]):
-        consensus+= "C"
-    elif (T[k]>A[k]) and (T[k]>G[k]) and (T[k]>C[k]):
-        consensus+= "T"
-    elif (G[k]>T[k]) and (G[k]>C[k]) and (G[k]>A[k]):
-        consensus+= "G"
+    if A[k] >= max(T[k], C[k], G[k]):
+        consensus += "A"
+    elif C[k] >= max(A[k], T[k], G[k]):
+        consensus += "C"
+    elif G[k] >= max(A[k], T[k], C[k]):
+        consensus += "G"
     else:
-        if max(A[k], C[k], G[k], T[k]) in A:
-            consensus+= "A"
-        elif max(A[k], C[k], G[k], T[k]) in C:
-            consensus+= "C"
-        elif max(A[k], C[k], G[k], T[k]) in T:
-            consensus+= "T"
-        elif max(A[k], C[k], G[k], T[k]) in G:
-            consensus+= "G"
+        consensus += "T"
 print(consensus, " ") 
 
 
@@ -74,5 +57,5 @@ print("C:", (" ".join(str(x) for x in C)), " ")
 print("G:", (" ".join(str(x) for x in G)), " ")
 print("T:", (" ".join(str(x) for x in T)))
 
-print(len(consensus))
-print(columns)
+#print(len(consensus))
+#print(columns)
