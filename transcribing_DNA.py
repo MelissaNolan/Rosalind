77fa330357d2ed685d112f81cd1e28e9b47f8f16
Melissa Nolan <125099416+MelissaNolan@users.noreply.github.com>
2025-02-18 08:08:48 -0800

    Update transcribing_DNA.py
    Add Rosalind link

--- transcribing_DNA.py
+++ transcribing_DNA.py
@@ -1,4 +1,7 @@
+#Solution to Rosalind problem "Transcribind DNA into RNA" https://rosalind.info/problems/rna/
+
 #Replaces T from DNA with U in resulting RNA
+
 file = open("/Users/melissanolan/Downloads/rosalind_rna.txt")
 
 DNA = str(file.read())
