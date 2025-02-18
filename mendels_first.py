8294729746317cb99cf245c6c63949574c2a7454
Melissa Nolan <125099416+MelissaNolan@users.noreply.github.com>
2025-02-18 07:58:42 -0800

    Update mendels_first.py
    Add comments and Rosalind link

--- mendels_first.py
+++ mendels_first.py
@@ -1,8 +1,10 @@
-k=19
-m=23
-n=16
+#Solution to Rosalind problem "Mendel's First Law" https://rosalind.info/problems/iprb/
 
-total=m+n+k
+k = 19 #homozygous dominant
+m = 23 #heterozygous
+n = 16 #homozygous recessive
+
+total = m + n + k
 
 #find chance of each mate
 
@@ -12,5 +14,7 @@ hetero_rec_mate = ((n/(total))*(m/(total-1)) + (m/(total))*(n/(total-1))) *0.5
 chance_rec = rec_mate + hetero_mate + hetero_rec_mate
 
 #chance of dominant allele is 1 - chance of recessive allele
+#The probability that two randomly selected mating organisms will produce an individual possessing a dominant allele
+# any two organisms can mate.
 
 print(1-chance_rec)
