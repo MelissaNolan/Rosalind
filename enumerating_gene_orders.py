3c42289b8aee78cc6cf5dfd514339232ffc45548
Melissa Nolan <125099416+MelissaNolan@users.noreply.github.com>
2025-02-18 07:38:49 -0800

    Update enumerating_gene_orders.py
    Add Rosalind link

--- enumerating_gene_orders.py
+++ enumerating_gene_orders.py
@@ -1,12 +1,16 @@
+#Solution to Rosalind problem "Enumerating Gene Orders" https://rosalind.info/problems/perm/
+
 from itertools import permutations
 n=6
 
 #create a list of integers from 1 to n
+
 orders=[]
 for i in range(1, n+1):
     orders.append(i)
 
 #get permutations
+
 perm = list(permutations(orders))
 print(len(perm))
 
