757e65504084b9ac28cf81057c68b77023e29d10
Melissa Nolan <125099416+MelissaNolan@users.noreply.github.com>
2025-02-18 07:42:14 -0800

    Update expected_offspring.py
    Add comments and Rosalind link

--- expected_offspring.py
+++ expected_offspring.py
@@ -1,9 +1,15 @@
-a=18260
-b=19745
-c=18703
-d=16135
-e=19626
-f=16327
+#Solution to Rosalind problem "Calculating Expected Offspring" https://rosalind.info/problems/iev/
+
+#Individuals with each phenotype in a population 
+
+a=18260 #AA-AA
+b=19745 #AA-Aa
+c=18703 #AA-aa
+d=16135 #Aa-Aa
+e=19626 #Aa-aa
+f=16327 #aa-aa
+
+#formula for dominant phenotype presentation in next gen, given each couple has 2 offspring
 
 expected= 2*(a+b+c+0.75*d+0.5*e)
 print(expected)
